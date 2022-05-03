2012c5faf68b8fffd13491b9505cb3b8a5762188
Professor Rodrigo Bossini <professorbossini@gmail.com>
2022-05-03 10:15:42 -0300

    proc cadastro de cliente

--- restaurante.sql
+++ restaurante.sql
@@ -46,6 +46,24 @@ CREATE TABLE IF NOT EXISTS tb_item_pedido(
 	CONSTRAINT fk_pedido FOREIGN KEY (cod_pedido) REFERENCES tb_pedido(cod_pedido)
 	--(1, 4), (1, 4)
 );
+-------------------------------------------------------------------------------------
+-- cadastro de cliente
+-- vejamos como funciona um parâmetro com valor default
+CREATE OR REPLACE PROCEDURE sp_cadastrar_cliente (IN nome VARCHAR(200), IN codigo INT DEFAULT NULL)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	IF codigo IS NULL THEN
+		INSERT INTO tb_cliente (nome) VALUES (nome);
+	ELSE
+		INSERT INTO tb_cliente (codigo, nome) VALUES (codigo, nome);
+	END IF;
+END;
+$$
+
+CALL sp_cadastrar_cliente ('Joáo da Silva');
+CALL sp_cadastrar_cliente ('Maria Santos');
+
 
 
 
