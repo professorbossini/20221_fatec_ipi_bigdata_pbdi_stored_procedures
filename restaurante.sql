77ff907c66fff074ad11789ef4f3a8ee6f718232
Professor Rodrigo Bossini <professorbossini@gmail.com>
2022-05-03 11:11:42 -0300

    proc de fechamento de pedido

--- restaurante.sql
+++ restaurante.sql
@@ -129,6 +129,29 @@ BEGIN
 	RAISE NOTICE 'Total do pedido %: R$ %', cod_pedido, valor_total;
 END;
 $$
+-------------------------------------------------------------------------------------
+-- fechar um pedido
+CREATE OR REPLACE PROCEDURE sp_fechar_pedido (IN valor_a_pagar INT, IN cod_pedido INT)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	valor_total INT;
+BEGIN
+	--vamos verificar se o valor_a_pagar é suficiente
+	CALL sp_calcular_valor_de_um_pedido (cod_pedido, valor_total);
+	IF valor_a_pagar < valor_total THEN
+		RAISE 'R$% insuficiente para pagar a conta de R$%', valor_a_pagar, valor_total;
+	ELSE
+		UPDATE tb_pedido p SET
+		data_modificacao = CURRENT_TIMESTAMP,
+		status = 'fechado'
+		WHERE p.cod_pedido = $2;
+	END IF;
+END;
+$$
+SELECT * FROM tb_pedido;
+
+CALL sp_fechar_pedido(100, 1);
 
 
 
