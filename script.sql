cb88e9441563129f1b6fca98606c3c2d1b09c69a
Professor Rodrigo Bossini <professorbossini@gmail.com>
2022-05-03 09:09:23 -0300

    Exemplo parâmetro INOUT

--- script.sql
+++ script.sql
@@ -68,6 +68,27 @@ BEGIN
 END;
 $$
 
+--DROP PROCEDURE IF EXISTS sp_acha_maior;
+CREATE OR REPLACE PROCEDURE sp_acha_maior (INOUT valor1 INT, IN valor2 INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	IF valor2 > valor1 THEN
+		-- 2 := valor2;
+		valor1 := valor2;
+	END IF;
+END;
+$$
+
+DO $$
+DECLARE
+	valor1 INT := 2;
+	valor2 INT := 3;	
+BEGIN
+	CALL sp_acha_maior(valor1, valor2);
+	RAISE NOTICE '% é o maior', valor1;
+END;
+$$
 
 
 
